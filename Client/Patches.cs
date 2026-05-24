14a81a9f1e684823842fa766d3ba44ab79f1efbf
7Bpencil <Edward.Ekb@yandex.ru>
2026-05-24 08:30:01 +0500

    hide stuff hanging off the invisible ceiling

--- Client/Patches.cs
+++ Client/Patches.cs
@@ -6,12 +6,16 @@
 //
 
 using EFT;
+using EFT.Interactive;
 using EFT.Hideout;
 using SevenBoldPencil.Common;
 using System;
+using System.Collections.Generic;
 using System.Reflection;
 using SPT.Reflection.Patching;
 using HarmonyLib;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace SevenBoldPencil.HideoutSky
 {
@@ -29,6 +33,20 @@ namespace SevenBoldPencil.HideoutSky
         }
 	}
 
+	public struct LightLevel_Proxy
+	{
+		private static TypedFieldInfo<LightLevel, List<GInterface467>> __lightSwitchers = new("_lightSwitchers");
+
+		public List<GInterface467> _lightSwitchers { get { return __lightSwitchers.Get(__instance); } set { __lightSwitchers.Set(__instance, value); } }
+
+        private LightLevel __instance;
+
+        public LightLevel_Proxy(LightLevel instance)
+        {
+            __instance = instance;
+        }
+	}
+
 	public class Patch_HideoutController_HideoutAwake : ModulePatch
 	{
         protected override MethodBase GetTargetMethod()
@@ -37,11 +55,91 @@ namespace SevenBoldPencil.HideoutSky
         }
 
         [PatchPostfix]
-        public static void Postfix(HideoutCustomizationItemsInstaller ___CustomizationItemsInstaller)
+        public static void Postfix(HideoutController __instance, HideoutCustomizationItemsInstaller ___CustomizationItemsInstaller)
 		{
 			var ceiling = new HideoutCustomizationItemsInstaller_Proxy(___CustomizationItemsInstaller)._ceilingPoint;
 			ceiling.gameObject.SetActive(false);
+
+			foreach (var (areaType, area) in __instance.Areas)
+			{
+				var areaLevel = area.CurrentLevel;
+				if (!areaLevel)
+				{
+					continue;
+				}
+				foreach (var lightLevel in areaLevel.LightingLevels.Values)
+				{
+					if (!lightLevel)
+					{
+						continue;
+					}
+					var lightSwitchers = new LightLevel_Proxy(lightLevel)._lightSwitchers;
+					foreach (var lightSwitcher in lightSwitchers)
+					{
+						HideLightSwitcherMesh(lightSwitcher);
+					}
+				}
+				if (areaType == EAreaType.Security)
+				{
+					var securityRoot = areaLevel.HighlightTransform;
+					var childCount = securityRoot.childCount;
+					for (var i = 0; i < childCount; i++)
+					{
+						var child = securityRoot.GetChild(i);
+						if (child.name.Contains("metall_patch"))
+						{
+							DisableAllCeilingMeshRenderers(child);
+						}
+					}
+				}
+			}
+
+			var hideoutScene = SceneManager.GetSceneByName(SceneResourceKeyAbstractClass.HideoutSceneName);
+			if (hideoutScene.IsValid())
+			{
+				foreach (var root in hideoutScene.GetRootGameObjects())
+				{
+					if (root.name == "SceneContent")
+					{
+						var baseLamps = root.transform.Find("cut_lamp");
+						if (baseLamps)
+						{
+							DisableAllCeilingMeshRenderers(baseLamps);
+						}
+					}
+					if (root.name == "!wires_light")
+					{
+						root.SetActive(false);
+					}
+				}
+			}
+
 			Plugin.Instance.LoadSkybox();
+		}
+
+		public static void HideLightSwitcherMesh(GInterface467 lightSwitcher)
+		{
+			// lightSwitcher can be: CandleSwitcher, GarlandSwitcher, LampController,
+			// but only garland and lamps can be on the ceiling
+			if (lightSwitcher is GarlandSwitcher garland)
+			{
+				DisableAllCeilingMeshRenderers(garland);
+			}
+			else if (lightSwitcher is LampController lamp)
+			{
+				DisableAllCeilingMeshRenderers(lamp);
+			}
+		}
+
+		public static void DisableAllCeilingMeshRenderers(Component component)
+		{
+			foreach (var meshRenderer in component.GetComponentsInChildren<MeshRenderer>())
+			{
+				if (meshRenderer.transform.position.y > 2.8f)
+				{
+					meshRenderer.enabled = false;
+				}
+			}
 		}
 	}
 
