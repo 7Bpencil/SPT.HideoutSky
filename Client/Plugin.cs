697d37b5b2a675e8440f16466143110fba9a04d2
7Bpencil <Edward.Ekb@yandex.ru>
2026-07-29 01:11:55 +0500

    bump version number

--- Client/Plugin.cs
+++ Client/Plugin.cs
@@ -29,7 +29,7 @@ namespace SevenBoldPencil.HideoutSky
         public Color OriginalAmbientColor;
     }
 
-    [BepInPlugin("7Bpencil.HideoutSky", "7Bpencil.HideoutSky", "1.0.1")]
+    [BepInPlugin("7Bpencil.HideoutSky", "7Bpencil.HideoutSky", "1.1.0")]
     public class Plugin : BaseUnityPlugin
     {
         public static readonly int _Tint = Shader.PropertyToID("_Tint");
